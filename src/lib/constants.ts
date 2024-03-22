2a5ac73c6a8ef8582788747f18210ff07984ce4b
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-22 14:53:20 +0100

    create validators, types and constant file

---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const STARTER_USER_AVATAR_URL = 'https://i.pinimg.com/280x280_RS/77/0f/b7/770fb75f5e81e4c2dbe8934f246aeeab.jpg';
+export const GOOGLE_IMG_URL = "https://cdn-icons-png.flaticon.com/256/1199/1199414.png"
---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export type NewUser = {
+  username: string;
+  email: string;
+  password: string;
+};
+export type User = {
+  accountId: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  providerId?:string
+};
+export type LoginUser = {
+  email: string;
+  password: string;
+};
+export type AuthContextType = {
+  currentUser: User;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User>>;
+  isLoading: boolean;
+  isAuth: boolean;
+};
