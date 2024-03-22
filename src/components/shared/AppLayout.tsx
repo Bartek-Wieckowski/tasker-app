8360b0683727a11ca2d7365d5ff19b39ecc56b83
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-22 14:50:04 +0100

    add isAuth from AuthContext to manage content

--- src/components/shared/AppLayout.tsx
+++ src/components/shared/AppLayout.tsx
@@ -3,15 +3,23 @@ import Container from './Container';
 import Logo from './Logo';
 import Header from './Header';
 import UserNavigation from './UserNavigation';
+import { useAuth } from '@/contexts/AuthContext';
+import Loader from './Loader';
+import UserPanel from './UserPanel';
 
 const AppLayout = () => {
-  const isAuth = false;
+  const { isLoading: isLoadingAuth, isAuth } = useAuth();
+
+  if (isLoadingAuth) {
+    return <Loader />;
+  }
+
   return (
     <section className="flex flex-col justify-center max-w-7xl w-full mx-auto p-2">
       <Container>
         <Header>
           <Logo />
-          {!isAuth && <UserNavigation />}
+          {isAuth ? <UserPanel /> : <UserNavigation />}
         </Header>
       </Container>
       <Container>
