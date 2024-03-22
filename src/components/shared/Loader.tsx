7b56f63f64397705a62d9e5ebd0c27865cd040a5
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-22 14:50:25 +0100

    create loader component

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { Loader as LoaderIcon } from 'lucide-react';
 
 const Loader = () => {
   return (
-    <div>
-      Loader component
+    <div className="flex justify-center items-center w-full">
+      <LoaderIcon className="animate-spin" />
     </div>
   );
-}
+};
 
 export default Loader;
