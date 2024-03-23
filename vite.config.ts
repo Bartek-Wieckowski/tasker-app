1c6a41a907554110b7f10d3cd0b8288c43981d36
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-23 18:08:43 +0100

    comment line

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    include: ['esm-dep > cjs-dep'],
-  },
+  // optimizeDeps: {
+  //   include: ['esm-dep > cjs-dep'],
+  // },
 })
