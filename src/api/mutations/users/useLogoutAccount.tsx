ea1744e9bc72f52b0790f7f0a7284a71236811ce
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-23 18:00:26 +0100

    add onSuccess and onError

--- src/api/mutations/users/useLogoutAccount.tsx
+++ src/api/mutations/users/useLogoutAccount.tsx
@@ -1,13 +1,24 @@
 import { useMutation } from '@tanstack/react-query';
 import { logoutAccount } from '@/api/apiUsers';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/routes/constants';
+import { useToast } from '@/components/ui/use-toast';
 
 export function useLogoutAccount() {
+  const navigate = useNavigate();
+  const { toast } = useToast();
   const {
     isPending: isLogouting,
     isError,
     mutateAsync: logoutUser,
   } = useMutation({
     mutationFn: logoutAccount,
+    onSuccess: () => {
+      navigate(ROUTES.home);
+    },
+    onError: () => {
+      toast({ title: 'Logout failed. Please try again.', variant: 'destructive' });
+    },
   });
 
   return { logoutUser, isLogouting, isError };
