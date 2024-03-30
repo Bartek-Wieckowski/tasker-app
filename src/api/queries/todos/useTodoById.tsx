126685b5d49e02932030adc1f59ec83a7272c0be
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-30 23:23:27 +0100

    create all queries in todos

---
+++ src/api/queries/todos/useTodoById.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { getTodoById } from '@/api/apiTodos';
+import { User } from '@/types/types';
+import { QUERY_KEYS } from '@/api/constants';
+export const useTodoById = (todoId: string, selectedDate: string, currentUser: User) => {
+  const {
+    data: todo,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: [QUERY_KEYS.todos, todoId],
+    queryFn: () => getTodoById(todoId, selectedDate, currentUser),
+    enabled: !!todoId,
+  });
+
+  return { todo, isLoading, isError };
+};
