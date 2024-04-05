87da0f428279a965cee4df5a6fadeb2aeb90846e
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-04-05 23:24:21 +0200

    add tabs text in const variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,6 @@
 export const STARTER_USER_AVATAR_URL = 'https://i.pinimg.com/280x280_RS/77/0f/b7/770fb75f5e81e4c2dbe8934f246aeeab.jpg';
 export const GOOGLE_IMG_URL = "https://cdn-icons-png.flaticon.com/256/1199/1199414.png"
+
+export const TABS_TEXT_1 = "All"
+export const TABS_TEXT_2 = "Completed"
+export const TABS_TEXT_3 = "Uncompleted"
