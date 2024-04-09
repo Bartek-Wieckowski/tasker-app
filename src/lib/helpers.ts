d79d34951324291bdff0249c4d2dc70d520b4f81
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-04-09 18:38:19 +0200

    update helpers date functions

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,45 +3,57 @@ export function dateCustomFormatting(date: Date): string {
   return `${padStart(date.getDate())}-${padStart(date.getMonth() + 1)}-${date.getFullYear()}`;
 }
 
-// export function formatDateString(dateString: string) {
-//   const options: Intl.DateTimeFormatOptions = {
-//     year: 'numeric',
-//     month: 'short',
-//     day: 'numeric',
-//   };
+export function formatDateString(dateString: string | Date) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
 
-//   const date = new Date(dateString);
-//   const formattedDate = date.toLocaleDateString('en-US', options);
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString('en-US', options);
 
-//   const time = date.toLocaleTimeString([], {
-//     hour: 'numeric',
-//     minute: '2-digit',
-//   });
+  const time = date.toLocaleTimeString([], {
+    hour: 'numeric',
+    minute: '2-digit',
+  });
 
-//   return `${formattedDate} at ${time}`;
-// }
+  return `${formattedDate} at ${time}`;
+}
 
-export const multiFormatDateString = (timestamp: any = ''): string => {
-  if (timestamp && timestamp.seconds && timestamp.nanoseconds) {
-    const seconds = timestamp.seconds * 1000;
-    const nanoseconds = timestamp.nanoseconds / 1000000;
-    const milliseconds = seconds + nanoseconds;
-    timestamp = new Date(milliseconds);
-  }
-
-  const date: Date = new Date(timestamp);
+export const multiFormatDateString = (timestamp: Date | { seconds: number; nanoseconds: number } = new Date()): string => {
+  const date: Date = convertTimestampToDate(timestamp);
   const now: Date = new Date();
 
   const diff: number = now.getTime() - date.getTime();
   const diffInSeconds: number = Math.abs(diff) / 1000;
   const diffInMinutes: number = diffInSeconds / 60;
   const diffInHours: number = diffInMinutes / 60;
+  const diffInDays: number = diffInHours / 24;
 
-  if (diffInHours >= 1) {
-    return `${Math.floor(diffInHours)} ${Math.floor(diffInHours) === 1 ? 'hour' : 'hours'} ago`;
-  } else if (diffInMinutes >= 1) {
-    return `${Math.floor(diffInMinutes)} ${Math.floor(diffInMinutes) === 1 ? 'minute' : 'minutes'} ago`;
-  } else {
-    return 'Just now';
+  switch (true) {
+    case Math.floor(diffInDays) >= 30:
+      return formatDateString(date);
+    case Math.floor(diffInDays) === 1:
+      return `${Math.floor(diffInDays)} day ago`;
+    case Math.floor(diffInDays) > 1 && diffInDays < 30:
+      return `${Math.floor(diffInDays)} days ago`;
+    case Math.floor(diffInHours) >= 1:
+      return `${Math.floor(diffInHours)} hours ago`;
+    case Math.floor(diffInMinutes) >= 1:
+      return `${Math.floor(diffInMinutes)} minutes ago`;
+    default:
+      return 'Just now';
   }
 };
+
+export function convertTimestampToDate(timestamp: Date | { seconds: number; nanoseconds: number }): Date {
+  if (timestamp instanceof Date) {
+    return timestamp;
+  } else {
+    const seconds = timestamp.seconds * 1000;
+    const nanoseconds = timestamp.nanoseconds / 1000000;
+    const milliseconds = seconds + nanoseconds;
+    return new Date(milliseconds);
+  }
+}
