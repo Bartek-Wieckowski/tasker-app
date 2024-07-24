d2e4d68c1d2fe792bf7cc5e78e4383956e2da09a
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-07-24 20:00:50 +0200

    update test code

--- __tests__/components/logo/Logo.test.tsx
+++ __tests__/components/logo/Logo.test.tsx
@@ -1,8 +1,9 @@
 import Logo from '@/components/shared/Logo';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { ROUTES } from '@/routes/constants';
+import { render } from '../../setup';
 
 describe('Logo component', () => {
   it('should render Logo component', () => {
