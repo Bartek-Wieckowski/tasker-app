ee27001766083333d301f63c874b6abdec972c6f
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-08-06 15:15:07 +0200

    first part of refactoring this file

--- src/api/apiTodos.ts
+++ src/api/apiTodos.ts
@@ -1,86 +1,88 @@
+import { FILES_FOLDER_todoImages, TABLE_NAME_taskerUserTodos } from '@/lib/constants';
 import { db, storage } from '@/lib/firebase.config';
+import { getFirestoreDocRef } from '@/lib/firebaseHelpers';
 import { convertTimestampToDate } from '@/lib/helpers';
-import { TodoItem, TodoItemDetails, TodoItemDetailsGlobalSearch, User } from '@/types/types';
-import { doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
+import { TodoItem, TodoItemBase, TodoItemDetails, TodoItemDetailsGlobalSearch, User } from '@/types/types';
+import { doc, getDoc, updateDoc, setDoc, DocumentSnapshot, DocumentReference, DocumentData } from 'firebase/firestore';
 import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
-export async function getTodosFromDay(selectedDate: string, currentUser: User): Promise<TodoItemDetails[]> {
-  const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
-  const docSnapshot = await getDoc(docRef);
-
-  if (docSnapshot.exists()) {
-    const userData = docSnapshot.data();
-    if (userData[selectedDate]?.userTodosOfDay) {
-      return userData[selectedDate].userTodosOfDay;
-    } else {
-      return [];
-    }
-  } else {
-    return [];
-  }
+export async function getTodosFromDay(selectedDate: string, currentUser: User) {
+  const docSnapshot = await getUserTodosDocSnapshot(currentUser.accountId);
+  return getTodosForDate(selectedDate, docSnapshot);
 }
 
-export async function getTodoById(todoId: string, selectedDate: string, currentUser: User): Promise<TodoItemDetailsGlobalSearch | undefined> {
-  const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
-  const docSnapshot = await getDoc(docRef);
-
-  if (docSnapshot.exists()) {
-    const userData = docSnapshot.data();
-    if (userData[selectedDate]?.userTodosOfDay) {
-      const todosOfDay = userData[selectedDate].userTodosOfDay;
-      const todo = todosOfDay.find((todo: TodoItemDetailsGlobalSearch) => todo.id === todoId);
-      return todo;
-    } else {
-      return undefined;
-    }
-  } else {
-    return undefined;
-  }
+export async function getUserTodosDocSnapshot(accountId: string) {
+  const userTodosDocRef = getFirestoreDocRef(TABLE_NAME_taskerUserTodos, accountId);
+  return await getDoc(userTodosDocRef);
 }
 
-export async function addTodo(todoDetails: TodoItem, selectedDate: string, currentUser: User) {
-  const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
-  const docSnapshot = await getDoc(docRef);
+export function getTodosForDate(selectedDate: string, docSnapshot: DocumentSnapshot) {
+  if (!docSnapshot.exists()) return [];
 
-  const todoId = crypto.randomUUID();
-  const timestamp = new Date();
+  const userData = docSnapshot.data();
+  const dayData = userData[selectedDate];
+  return dayData?.userTodosOfDay ?? [];
+}
 
-  let imageUrl = '';
+export async function getTodoById(todoId: string, selectedDate: string, currentUser: User) {
+  const docSnapshot = await getUserTodosDocSnapshot(currentUser.accountId);
+  const todosOfDay = getTodosForDate(selectedDate, docSnapshot);
 
-  if (todoDetails.imageUrl) {
-    const imageRef = ref(storage, `todoImages/${currentUser.accountId}/${selectedDate}_${todoId}`);
-    const uploadTask = uploadBytesResumable(imageRef, todoDetails.imageUrl as File);
+  const todo = todosOfDay.find((todo: TodoItemBase) => todo.id === todoId);
+  return todo;
+}
 
-    await uploadTask;
+export async function uploadImageAndGetUrl(accountId: string, selectedDate: string, todoId: string, image: File) {
+  const imageRef = ref(storage, `${FILES_FOLDER_todoImages}/${accountId}/${selectedDate}_${todoId}`);
+  const uploadTask = uploadBytesResumable(imageRef, image);
+  await uploadTask;
+  return await getDownloadURL(uploadTask.snapshot.ref);
+}
 
-    imageUrl = await getDownloadURL(uploadTask.snapshot.ref);
-  }
-
-  const todoItem = {
+export function createTodoItem(todoDetails: TodoItem, todoId: string, imageUrl: string) {
+  return {
     id: todoId,
     todo: todoDetails.todo,
     todoMoreContent: todoDetails.todoMoreContent,
     imageUrl: imageUrl,
     isCompleted: false,
-    createdAt: timestamp,
-    updatedAt: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
+}
+
+export async function updateOrCreateTodos(docRef: DocumentReference, selectedDate: string, todoItem: TodoItemBase, userData: DocumentData) {
+  if (userData[selectedDate]?.userTodosOfDay) {
+    await updateDoc(docRef, {
+      [selectedDate]: {
+        userTodosOfDay: [...userData[selectedDate].userTodosOfDay, todoItem],
+      },
+    });
+  } else {
+    await updateDoc(docRef, {
+      [selectedDate]: {
+        userTodosOfDay: [todoItem],
+      },
+    });
+  }
+}
+
+export async function addTodo(todoDetails: TodoItem, selectedDate: string, currentUser: User) {
+  const docSnapshot = await getUserTodosDocSnapshot(currentUser.accountId);
+  const docRef = getFirestoreDocRef(TABLE_NAME_taskerUserTodos, currentUser.accountId);
+
+  const todoId = crypto.randomUUID();
+  let imageUrl = '';
+
+  if (todoDetails.imageUrl) {
+    imageUrl = await uploadImageAndGetUrl(currentUser.accountId, selectedDate, todoId, todoDetails.imageUrl as File);
+  }
+
+  const todoItem = createTodoItem(todoDetails, todoId, imageUrl);
 
   if (docSnapshot.exists()) {
     const userData = docSnapshot.data();
-    if (userData[selectedDate]?.userTodosOfDay) {
-      await updateDoc(docRef, {
-        [selectedDate]: {
-          userTodosOfDay: [...userData[selectedDate].userTodosOfDay, todoItem],
-        },
-      });
-    } else {
-      await updateDoc(docRef, {
-        [selectedDate]: {
-          userTodosOfDay: [todoItem],
-        },
-      });
-    }
+    await updateOrCreateTodos(docRef, selectedDate, todoItem, userData);
   } else {
     await setDoc(docRef, {
       [selectedDate]: {
@@ -92,7 +94,7 @@ export async function addTodo(todoDetails: TodoItem, selectedDate: string, curre
 }
 
 export async function editTodo(todoId: string, newTodoDetails: Partial<TodoItemDetails>, selectedDate: string, currentUser: User, deleteImage: boolean) {
-  const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
+  const docRef = doc(db, TABLE_NAME_taskerUserTodos, currentUser.accountId);
   const docSnapshot = await getDoc(docRef);
 
   let newImageUrl = '';
@@ -143,7 +145,7 @@ export async function editTodo(todoId: string, newTodoDetails: Partial<TodoItemD
 }
 
 export async function updateTodoCompletionStatus(todoId: string, selectedDate: string, currentUser: User, isCompleted: boolean) {
-  const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
+  const docRef = doc(db, TABLE_NAME_taskerUserTodos, currentUser.accountId);
   const docSnapshot = await getDoc(docRef);
 
   if (docSnapshot.exists()) {
@@ -166,7 +168,7 @@ export async function updateTodoCompletionStatus(todoId: string, selectedDate: s
 }
 
 export async function deleteTodo(todoId: string, selectedDate: string, currentUser: User) {
-  const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
+  const docRef = doc(db, TABLE_NAME_taskerUserTodos, currentUser.accountId);
   const docSnapshot = await getDoc(docRef);
 
   if (docSnapshot.exists()) {
@@ -191,7 +193,7 @@ export async function deleteTodo(todoId: string, selectedDate: string, currentUs
 
 export async function searchInDatabase(searchValue: string, currentUser: User) {
   try {
-    const docRef = doc(db, 'taskerUserTodos', currentUser.accountId);
+    const docRef = doc(db, TABLE_NAME_taskerUserTodos, currentUser.accountId);
     const docSnapshot = await getDoc(docRef);
 
     const userData = docSnapshot.data();
