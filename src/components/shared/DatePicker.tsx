e3e70ecf9c8e39e0083a92b73cd3208de7c33e5e
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-12-18 05:10:58 +0100

    Refactor DatePicker component for improved readability and structure

--- src/components/shared/DatePicker.tsx
+++ src/components/shared/DatePicker.tsx
@@ -4,7 +4,11 @@ import { Calendar as CalendarIcon } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import { dateCustomFormatting } from '@/lib/helpers';
 import { useAuth } from '@/contexts/AuthContext';
 import { useGlobalSearch } from '@/contexts/GlobalSearchContext';
@@ -12,7 +16,8 @@ import { useGlobalSearch } from '@/contexts/GlobalSearchContext';
 export function DatePicker() {
   const [date, setDate] = useState<Date>(new Date());
   const { setSelectedDate } = useAuth();
-  const { setIsGlobalSearch, setSearchValueGlobal, setGlobalSearchResult } = useGlobalSearch();
+  const { setIsGlobalSearch, setSearchValueGlobal, setGlobalSearchResult } =
+    useGlobalSearch();
 
   useEffect(() => {
     if (date) {
@@ -30,13 +35,24 @@ export function DatePicker() {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button variant={'outline'} className={cn('w-[150px] justify-center text-left font-normal', !date && 'text-muted-foreground')}>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-full justify-center text-left font-normal',
+            !date && 'text-muted-foreground'
+          )}
+        >
           <CalendarIcon className="mr-2 h-4 w-4 shrink-0" />
           {date ? format(date, 'PPP') : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
-        <Calendar mode="single" selected={date} onDayClick={handleDateSelect} initialFocus />
+        <Calendar
+          mode="single"
+          selected={date}
+          onDayClick={handleDateSelect}
+          initialFocus
+        />
       </PopoverContent>
     </Popover>
   );
