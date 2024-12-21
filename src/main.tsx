c83df9abc33bc0915ad4f2597e4bc588a63a1f58
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-12-21 11:21:37 +0100

    Refactor main.tsx for improved code formatting and readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import ErrorBoundary from './components/shared/ErrorBoundary'
-import { Toaster } from './components/ui/toaster'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import ErrorBoundary from './components/shared/ErrorBoundary';
+import { Toaster } from './components/ui/toaster';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,5 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
       <Toaster />
     </ErrorBoundary>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
