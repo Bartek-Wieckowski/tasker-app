8f52258d1ccd27e41b2361ca7b36c96de40235b2
Bartek Wieckowski <bart.it.pro@gmail.com>
2025-01-01 08:03:26 +0100

    chore: update service worker revision for improved caching

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bt81hf1g2i"
+    "revision": "0.aisosqr2qvg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
